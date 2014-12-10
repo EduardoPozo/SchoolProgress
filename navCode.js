6472e3f734c87e131ebacee320e23447101b1c4b
EduardoPozo <pozo.eduardo@yahoo.com>
2014-12-10 13:34:59 -0500

    Adding files for the website, I believe error was fixed for AJAX

--- navCode.js
+++ navCode.js
@@ -9,14 +9,15 @@ Created: 11/05/2014
 Last Modified: 11/05/2014
  */
 function getContent(fragmentId, callback){
-  //references to each state the site can be in
-  var partials = {
-    home: "Welcome to the home page.  This assignment required us to create a single page app.",
-    about: "The assignment had us use the #value of pages to load the content of a new page.",
-    contact: "If you have any questions about how the #value method works please email me at eduardo_pozo@student.uml.edu"
+  //create a variable that gets a file
+  var request = new XMLHttpRequest();
+  
+  //callback function using the text from the variable that holds a file
+  request.onload = function(){
+    callback(request.responseText);
   };
-  //Return the content
-  callback(partials[fragmentId]);
+  request.open("GHT", fragmentId + ".html");
+  request.send(null);
 }
 //This function changes the active link of a page by cycling through until it finds the correct #value.
 function setActiveLink(fragmentId){
@@ -41,17 +42,48 @@ function navigate(){
   var contentDiv = document.getElementById("content");
   
   fragmentId = location.hash.substr(1);
-  //uses send fragmentId and and a function that set content divs content
-  getContent(fragmentId, function(content){
-    contentDiv.innerHTML=content;
-  });
+  
+  //Adding an if statement so that it renders the JSON file if #blog is selected
+  if(fragmentId === blog){
+    //since this method doesn't use the call back function I need to declare content
+    var content = "";
+    //reads the JSon file, this AJAX set up was borrowed from Jesse Heines.
+    //His sample code can be found here
+    jQuery.ajax({
+      async: false,
+      dataType: "json",
+      url: "blog.json",
+      success: function(data){
+        blogPostInfo = data;
+      }
+    });
+    content += "<h1>" + blogPostInfo.title + "</h1>";
+    //loop through all the paragraphs and sentences for the content
+    for ( var para = 0 ; para < blogPostInfo.text.paragraphs.length ; para++ ) {
+      content += "<p class=\"firefox\">" ;
+      for ( var sent = 0 ; sent < blogPostInfo.text.paragraphs[para].length ; sent++ ) {
+        content += blogPostInfo.text.paragraphs[para][sent] + "&nbsp; " ;
+      }
+      content += "</p>" ;
+    }
+    //return the info from the JSON into the content div
+    contentDiv.innerHTML = content;
+  }//This is where Jesse Heine's code ends
+  else{
+    //uses send fragmentId and and a function that set content divs content
+    getContent(fragmentId, function(content){
+      contentDiv.innerHTML=content;
+    });
+  }
   //sets the new active link
   setActiveLink(fragmentId);
 }
-//For the initial value before ay hash value is loaded.
+//This is to display the initial value before any hash value has been loaded.
+//Calling navigate alone to set off this case.
 if(!location.hash){
   location.hash="#home";
 }
 navigate();
 
+//The event when you change the page
 window.addEventListener("hashchange",navigate);
